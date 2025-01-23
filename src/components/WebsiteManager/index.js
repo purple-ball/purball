e1c1c3f31fb5c4051cb9ce936a96bc43384b30c9
purple-ball <yjyyjyyjy2333@gmail.com>
2025-01-23 23:33:13 +0800

    7

--- src/components/WebsiteManager/index.js
+++ src/components/WebsiteManager/index.js
@@ -91,6 +91,15 @@ export default function WebsiteManager() {
         className="add-button"
         onClick={() => setShowAddForm(true)}
       >
+        <svg 
+          width="20" 
+          height="20" 
+          viewBox="0 0 20 20" 
+          fill="currentColor" 
+          style={{ marginRight: '8px' }}
+        >
+          <path d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z"/>
+        </svg>
         添加网站
       </button>
 
